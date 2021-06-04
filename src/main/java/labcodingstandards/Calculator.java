d0204a916c1a2d84a55af2e80a7a93a999a39c25
xmichelle26x <xmichelle26x@gmail.com>
2021-06-04 14:40:41 -0500

    Errors corrections and report with no errors or warnings

--- src/main/java/labcodingstandards/Calculator.java
+++ src/main/java/labcodingstandards/Calculator.java
@@ -1,80 +1,84 @@
-//
-//
+// Copyright (C) 2020
+// All rights reserved
 
 package labcodingstandards;
 
 import java.util.Scanner;
 
-
 /**
- * @author
+ * @author ADMIN
  *
  */
 public class Calculator {
-	
-	//CHECKSTYLE:OFF
-	public static void main(String[] args) {
+//CHECKSTYLE:OFF
+/**
+* @param args
+*/
+	public static void main(final String[] args) {
+
 		Scanner reader = new Scanner(System.in);
-		
-        System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
-        
-        char operator = reader.nextLine().charAt(0);
-        double First;
-        double second;
-        String input;
-        
-        while (true) {
-        	System.out.print("Enter first number: ");
-        	input = reader.nextLine();
-        	
-            try {
-            	First=Integer.parseInt(input);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Not valid!");
-            }
-        }
-        
-        while (true) {
-        	System.out.print("Enter second number: ");
-        	input = reader.nextLine();
-        	
-            try {
-            	second=Integer.parseInt(input);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Not valid!");
-            }
-        }
+		System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
+		char operator = reader.nextLine().charAt(0);
+		double first;
+		double second;
+		String input;
 
-        Calculator cal=new Calculator();
-        String result=cal.Operation(First,second,operator);
+		while (true) {
+			System.out.print("Enter first number: ");
+			input = reader.nextLine();
 
-        System.out.printf(result);
+			try {
+				first = Integer.parseInt(input);
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Not valid!");
+			}
+		}
+
+		while (true) {
+			System.out.print("Enter second number: ");
+			input = reader.nextLine();
+			try {
+				second = Integer.parseInt(input);
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Not valid!");
+			}
+		}
+
+		Calculator cal = new Calculator();
+		String result = cal.operation(first, second, operator);
+
+		System.out.printf(result);
 		reader.close();
-	}//CHECKSTYLE:ON
-	 
-	
+	}
+//CHECKSTYLE:ON
 
-	private String Operation(double first,double second,char operator) {
+	/**
+	 * @param first
+	 * @param second
+	 * @param operator
+	 * @return result
+	 */
+	private String operation(final double first,
+			final double second, final char operator) {
 		double result = 0;
-		switch(operator)
-        {
-            case '1':
-                result=first+second;
-                break;
-            case '2':
-                result=first-second;
-                break;
-            case '3':
-                result=first*second;
-                break;
-            case '4':
-                result =first/second;
-                break;
-            default:
-            	return "Error! operator is not correct";
-        }
-		return "The result is: "+result;
+		switch (operator) {
+		case '1':
+			result = first + second;
+			break;
+		case '2':
+			result = first - second;
+			break;
+		case '3':
+			result = first * second;
+			break;
+		case '4':
+			result = first / second;
+			break;
+		default:
+			return "Error! operator is not correct";
+		}
+		return "The result is: " + result;
 	}
 }
